b9c5e025fd25afeb2b847ad0e355b6494c774dfa
Zikora Ogbuagu <robbie89339@gmail.com>
2022-06-12 20:00:50 +0100

    feat: Added better steering animation

--- src/JetController.gd
+++ src/JetController.gd
@@ -7,13 +7,17 @@ export(float, 1.0, 10.0) var speed: float = 1.0
 var current_setup_index = 0
 
 func _ready():
-	animator.play("SteeringAnim")
+	animator.play("SteeringAnimation")
 
 func _process(delta):
+	# Switch camera setup with mouse clicks
+	if Input.is_action_just_pressed("switch_view"): on_switch_viewpoint()
+	# Loop animation (not sure why it's not looping on default)
+	if not animator.is_playing():
+		animator.play("SteeringAnimation")
+	# Go Beyond - Plus Ultra!
 	var motion_delta = Vector3.FORWARD * speed * delta
 	translate(motion_delta)
-	
-	if Input.is_action_just_pressed("switch_view"): on_switch_viewpoint()
 	
 func switch_viewpoint_to(index):
 	camera_setups[current_setup_index].get_child(0).set("current", false)
